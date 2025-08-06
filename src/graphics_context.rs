2365ec2674cd76c1bc4c7e99cda9fd633cc7c38f
German <germangolozubow@gmail.com>
2025-08-06 18:19:46 +0300

    Add wgpu initialization code

---
+++ src/graphics_context.rs
@@ -0,0 +1,82 @@
+mod surface_data;
+
+use crate::graphics_context::surface_data::SurfaceData;
+use anyhow::Context;
+use std::sync::Arc;
+use wgpu::{
+    Adapter, Device, DeviceDescriptor, Instance, InstanceDescriptor, PowerPreference, Queue,
+    RequestAdapterOptions,
+};
+use winit::dpi::{PhysicalPosition, PhysicalSize};
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowAttributes};
+
+pub struct GraphicsContext {
+    pub window: Arc<Window>,
+    pub instance: Instance,
+    pub adapter: Adapter,
+    pub device: Arc<Device>,
+    pub queue: Queue,
+    pub surface_data: SurfaceData,
+}
+
+impl GraphicsContext {
+    pub fn new(event_loop: &ActiveEventLoop) -> anyhow::Result<Self> {
+        let window = Arc::new(create_window(&event_loop)?);
+
+        let instance = Instance::new(&InstanceDescriptor::default());
+        let surface = instance
+            .create_surface(Arc::clone(&window))
+            .context("Failed to create surface")?;
+        let adapter =
+            futures::executor::block_on(instance.request_adapter(&RequestAdapterOptions {
+                power_preference: PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            }))
+            .context("Failed to request adapter")?;
+        let (device, queue) =
+            futures::executor::block_on(adapter.request_device(&DeviceDescriptor::default()))
+                .context("Failed to request device and queue")?;
+        let device = Arc::new(device);
+        let mut surface_data =
+            SurfaceData::new(Arc::clone(&window), surface, &adapter, Arc::clone(&device));
+        surface_data.configure(window.inner_size().width, window.inner_size().height);
+        window.request_redraw();
+
+        Ok(GraphicsContext {
+            window,
+            instance,
+            adapter,
+            device,
+            queue,
+            surface_data,
+        })
+    }
+}
+
+fn create_window(event_loop: &ActiveEventLoop) -> anyhow::Result<Window> {
+    let primary_monitor = event_loop
+        .primary_monitor()
+        .context("Failed to get primary monitor")?;
+
+    let monitor_size = primary_monitor.size();
+    let window_size = PhysicalSize::new(
+        (monitor_size.width as f32 * 0.75) as u32,
+        (monitor_size.height as f32 * 0.75) as u32,
+    );
+    let window_position = {
+        let x = (monitor_size.width / 2 - window_size.width / 2) as i32;
+        let y = (monitor_size.height / 2 - window_size.height / 2) as i32;
+        PhysicalPosition::new(x, y)
+    };
+
+    let window_attributes = WindowAttributes::default()
+        .with_min_inner_size(PhysicalSize::new(1, 1))
+        .with_inner_size(window_size)
+        .with_position(window_position);
+
+    event_loop
+        .create_window(window_attributes)
+        .context("Failed to create window")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,14 @@
-use anyhow::Context;
+mod graphics_context;
+
+use graphics_context::GraphicsContext;
 use winit::application::ApplicationHandler;
-use winit::dpi::{PhysicalPosition, PhysicalSize};
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowAttributes, WindowId};
+use winit::window::WindowId;
 
 #[derive(Default)]
 struct App {
     graphics_context: Option<GraphicsContext>,
-}
-
-struct GraphicsContext {
-    window: Window,
-}
-
-impl GraphicsContext {
-    fn new(event_loop: &ActiveEventLoop) -> anyhow::Result<Self> {
-        let primary_monitor = event_loop
-            .primary_monitor()
-            .context("Failed to get primary monitor")?;
-
-        let monitor_size = primary_monitor.size();
-        let window_size = PhysicalSize::new(
-            (monitor_size.width as f32 * 0.75) as u32,
-            (monitor_size.height as f32 * 0.75) as u32,
-        );
-        let window_position = {
-            let x = (monitor_size.width / 2 - window_size.width / 2) as i32;
-            let y = (monitor_size.height / 2 - window_size.height / 2) as i32;
-            PhysicalPosition::new(x, y)
-        };
-
-        let window_attributes = WindowAttributes::default()
-            .with_inner_size(window_size)
-            .with_position(window_position);
-
-        let window = event_loop
-            .create_window(window_attributes)
-            .context("Failed to create window")?;
-        Ok(GraphicsContext { window })
-    }
-}
-
-fn main() {
-    env_logger::init();
-
-    let event_loop = EventLoop::new().expect("Failed to create EventLoop");
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    event_loop
-        .run_app(&mut app)
-        .expect("Failed to run app in EventLoop");
 }
 
 impl ApplicationHandler for App {
@@ -74,13 +31,31 @@ impl ApplicationHandler for App {
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
         let graphics_context = self.graphics_context.as_mut().unwrap();
         match event {
-            WindowEvent::CloseRequested => {
-                event_loop.exit();
-            }
             WindowEvent::RedrawRequested => {
                 graphics_context.window.request_redraw();
+            }
+            WindowEvent::Resized(new_size) => {
+                graphics_context
+                    .surface_data
+                    .configure(new_size.width.max(1), new_size.height.max(1));
+                graphics_context.window.request_redraw();
+            }
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
             }
             _ => (),
         }
     }
+}
+
+fn main() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().expect("Failed to create EventLoop");
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop
+        .run_app(&mut app)
+        .expect("Failed to run app in EventLoop");
 }
